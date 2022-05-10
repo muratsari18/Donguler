2e0d3433b4f9bfbb7aeb5a6d914119cc8cf5dd67
muratsari18 <83982092+muratsari18@users.noreply.github.com>
2022-05-10 22:41:34 +0300

    Create girilenSayiBasamakToplami.java

---
+++ src/girilenSayiBasamakToplami.java
@@ -0,0 +1,17 @@
+import java.util.Scanner;
+public class GirilenSayininBasamakDegerToplam {
+    public static void main(String[] args) {
+        int number,numberValue,numberToplam=0;
+
+
+        Scanner scan = new Scanner(System.in);
+        System.out.print("Bir Sayi Giriniz: ");
+        number=scan.nextInt();
+        while (number!=0){
+            numberValue=number % 10;
+            numberToplam+=numberValue;
+            number=number/10;
+        }
+        System.out.println("Girdiginiz sayilarin toplami = " + numberToplam);
+        }
+}
