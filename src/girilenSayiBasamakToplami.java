d691a4ef48d3707b6106e4b64ff65202f2d21a3e
muratsari18 <83982092+muratsari18@users.noreply.github.com>
2022-05-14 19:31:15 +0300

    Update girilenSayiBasamakToplami.java

--- src/girilenSayiBasamakToplami.java
+++ src/girilenSayiBasamakToplami.java
@@ -8,9 +8,9 @@ public class GirilenSayininBasamakDegerToplam {
         System.out.print("Bir Sayi Giriniz: ");
         number=scan.nextInt();
         while (number!=0){
-            numberValue=number % 10;
-            numberToplam+=numberValue;
-            number=number/10;
+            numberValue=number % 10;     // sayının son basamak değerini alıyoruz
+            numberToplam+=numberValue;   //ve bu değerleri topluyoruz
+            number=number/10;            //Sayının basamak değeri azalıyor ve döngü tekrar başa dönüyor.
         }
         System.out.println("Girdiginiz sayilarin toplami = " + numberToplam);
         }
