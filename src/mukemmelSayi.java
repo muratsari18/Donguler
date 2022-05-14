9f2822e2f37f0f0ae64ca363ba3c0027399da6ca
muratsari18 <83982092+muratsari18@users.noreply.github.com>
2022-05-14 19:35:17 +0300

    Update mukemmelSayi.java

--- src/mukemmelSayi.java
+++ src/mukemmelSayi.java
@@ -8,7 +8,7 @@ public static void main (String[]args){
 		
 		for(int i=1; i<=number; i++) {
 			if(number%i==0)
-				total+=i;
+				total+=i;  // Tam bölünen değerleri topluyoruz ve totala atama yapıyoruz
 		}
 		if(total==(2*number))
 			System.out.println(number + " mükemmel bir sayıdır");
